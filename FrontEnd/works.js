91872ae8489035e2bb27cdb961ea14e2418751ac
Thomas Potherat <t.potherat@laposte.net>
2023-07-05 16:01:10 +0200

    Avancement sur les filtres

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,15 +1,18 @@
 
 
-
 // Récupération des travaux depuis l'API 
 fetch('http://localhost:5678/api/works')
     .then(reponse => reponse.json())
     .then((projects) => {
 
+const set = new Set (["Objets", "Appartements", "Hotels & restaurants"])
 
 for (let i=0; i < projects.length; i++) {
         const project = projects[i];
+   
         
+// New set afin de supprimer les doublons du array : 
+
 
 // Récupération de l'élément du DOM qui accueillera les projets
   const sectionGallery = document.querySelector(".gallery");
@@ -51,44 +54,61 @@ const buttonContainer = document.querySelector(".btn-container");
 
 const btnFilterAll = document.createElement("button");
 btnFilterAll.textContent = "Tous";
+btnFilterAll.id = "btnFilterAll";
 const btnFilterObjects = document.createElement("button");
 btnFilterObjects.textContent = "Objets";
+btnFilterObjects.id = "btnFilterObjects";
 const btnFilterAppartments = document.createElement("button");
 btnFilterAppartments.textContent = "Appartements";
+btnFilterAppartments.id = "btnFilterAppartments";
 const btnFilterRestaurants = document.createElement("button");
-btnFilterRestaurants.textContent = "Hôtels & restaurants";
+btnFilterRestaurants.textContent = "Hotels & restaurants";
+btnFilterRestaurants.id = "btnFilterRestaurants";
 
 
 
-
+// On rattache les balises à leur parent
 buttonContainer.appendChild(btnFilterAll);
 buttonContainer.appendChild(btnFilterObjects);
 buttonContainer.appendChild(btnFilterAppartments);
 buttonContainer.appendChild(btnFilterRestaurants);
 
- 
-   
+
+// Déclarer le texte du bouton en tant que catégorie bouton
 const categorieButton = [btnFilterAll.textContent, btnFilterObjects.textContent, btnFilterAppartments.textContent, btnFilterRestaurants.textContent];
 console.log(categorieButton)
 
+// Déclaration d'éléments
 const buttonFilter = document.querySelectorAll("button");
-const imageElement = document.querySelector("img")
+const blocFigure = document.querySelectorAll(".gallery figure")
 
 
 
-function onButtonClick (event)  {
-  console.log (event.currentTarget)
-  }
-  
+// Sélectionnez les éléments auxquels ajouter l'écouteur d'événement "click" :
+buttonFilter.forEach(button => {
+  button.addEventListener("click", function(event) {
+    const categorieButton = event.currentTarget.textContent;
+  console.log(categorieButton);
+// Effectuer le filtrage à l'intérieur de la fonction de rappel de l'écouteur d'évenement :
+    const categoriesFiltered = categories.filter(function(categorie) {
+      return categorie.name === categorieButton;
+    });
+    console.log(categoriesFiltered);
 
-buttonFilter.forEach(function(button) {
-  button.addEventListener("click", onButtonClick) 
-    console.log("clicked")
-    if (categorieButton === categories.name ) {
-      imageElement.style.display = "block";
-    } else {
-      imageElement.style.display = "none";
-    }
+    // OK JUSQU ICI
 
+    blocFigure.forEach(figure => {
+      const dataCategory = figure.category.name
+      console.log(dataCategory)
+
+      if (dataCategory) {
+        if (categoriesFiltered.includes(dataCategory)) {
+          figure.style.display = "block";
+        } else {
+          figure.style.display = "none";
+        }
+      }
+    });
+})
 })
 })
